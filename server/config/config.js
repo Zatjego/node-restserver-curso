336081872c517d3183bfafe31f49c9b73c75b070
zatjego <martinsktordie@gmail.com>
2020-07-02 17:17:45 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:admin@cluster0.jg2cx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 process.env.urlDB = urlDB;
